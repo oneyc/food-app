31fef4b5a25fae008d19e26e14075d44f11634b2
WanYC97 <61961954+WanYC97@users.noreply.github.com>
2022-04-13 19:09:27 +0800

    Header cart icon animation when meal is added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,30 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 import CartContext from "../../store/cart-context.js"
 
 const HeaderCartButton = (props) => {
+  const[btnAnimation, setBtnAnimation] = useState(false)
   const cartCtx = useContext(CartContext)  
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
   
+  const btnClasses = `${classes.button} ${btnAnimation? classes.bump : ''}`;
+
+  useEffect(()=> {
+    setBtnAnimation(true);
+    const aniTimer = setTimeout(()=> {setBtnAnimation(false)}, 300);
+    return(()=>{
+      clearTimeout(aniTimer)
+    })
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
